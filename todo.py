6b8cb32f15a80a41329a47c2e728b90aeb2e9bd9
Jason <hnhjknmn@gmail.com>
2026-06-22 04:28:22 -0400

    added task marking

--- todo.py
+++ todo.py
@@ -9,7 +9,7 @@ task_list = [task1, task2]
 def choose_action():
     print("1. View tasks")
     print("2. Add task")
-    print("3. Mark task as completed")
+    print("3. Mark task as complete or incomplete")
     print("4. Delete task")
     print("5. Exit")
 
@@ -25,10 +25,45 @@ def view_task():
         print(task)
         print("\n")
 
+def add_task():
+    print("Enter the task name")
+    name = input()
+    new_dict = {"title" : "name", "completed" : False}
+    task_list.append(new_dict)
+
+def mark_task():
+    print("Enter the number of the task you wish to mark")
+    while True:
+        try:
+            task_num = int(input()) - 1
+
+            if task_num <= len(task_list):
+                break
+            else:
+                print("Not a valid task number")
+
+        except ValueError:
+            print("Please enter a valid task number.")
+
+    valid_mark = ["completed", "incomplete"]
+    print(f"Do you wish to mark task {task_num} as complete or incomplete? Enter completed/incomplete")
+    mark_status = input().lower()
+
+    while mark_status not in valid_mark:
+        print("Not a valid answer, enter again.")
+        mark_status = input().lower()
+
+    if mark_status == "completed":
+        num = task_list[task_num]
+        num["completed"] = True
+    else:
+        num = task_list[task_num]
+        num["completed"] = False
+
 actions = {
-    1: view_task
-    # 2: add_task,
-    # 3: mark_task,
+    1: view_task,
+    2: add_task,
+    3: mark_task
     # 4: delete_task,
     # 5: exit_menu
 }
@@ -37,6 +72,6 @@ while running:
     selected = choose_action()
     actions[selected]()
 
-    running = False
+    # running = False
 
 
