2b9607f2e937cb9c952e7deaa31430da487ec034
Jason <hnhjknmn@gmail.com>
2026-06-24 05:22:31 -0400

    fixed bugs and handled some edge cases

--- todo.py
+++ todo.py
@@ -8,8 +8,6 @@ task_list = []
 
 file_path = Path(__file__).parent / "tasks.json"
 
-with open(file_path, "r") as file:
-    task_list = json.load(file)
 
 
 def choose_action():
@@ -21,10 +19,19 @@ def choose_action():
     print("5. Exit")
 
     valid = [1,2,3,4,5]
-    action = int(input())
-    while action not in valid:
-        print("Not a valid action, choose again")
-        action = int(input())
+
+    while True:
+        try:
+            action = int(input())
+
+            if action in valid:
+                break
+            else:
+                print("Not a valid action, choose again")
+
+        except ValueError:
+            print("Please enter a valid action number.")
+
     return action
 
 def view_task():
@@ -34,21 +41,29 @@ def view_task():
 
     for i, task in enumerate(task_list):
         if task["completed"] == False:
-            print(f"{i + 1}. [ ] {task["title"]}")
+            print(f"{i + 1}. [ ] {task['title']}")
         else:
-            print(f"{i + 1}. [x] {task["title"]}")
+            print(f"{i + 1}. [x] {task['title']}")
 
 def add_task():
+    
     view_task()
     print("Enter the task name or type cancel to go back")
-    name = input()
+    name = input().strip()
+
+    while not name:
+        print("Task name cannot be empty, enter again or type cancel to go back")
+        name = input().strip()
+
     if name.lower() == "cancel":
         return
-    else: 
-        new_dict = {"title" : name, "completed" : False}
-        task_list.append(new_dict)
+    new_dict = {"title" : name, "completed" : False}
+    task_list.append(new_dict)
 
 def mark_task():
+    if not task_list:
+        print("You don't have any tasks")
+        return
     view_task()
     print("Enter the number of the task you wish to mark or type -1 to exit")
     while True:
@@ -57,7 +72,7 @@ def mark_task():
             task_num2 = task_num - 1
             if task_num == -1:
                 return
-            elif task_num2 < len(task_list):
+            elif task_num2 < len(task_list) and task_num2 >= 0:
                 break
             else:
                 print("Not a valid task number")
@@ -66,7 +81,7 @@ def mark_task():
             print("Please enter a valid task number.")
 
     valid_mark = ["completed", "incomplete"]
-    print(f"Do you wish to mark task {task_num2} as complete or incomplete? Enter completed/incomplete")
+    print(f"Do you wish to mark task {task_num} as complete or incomplete? Enter completed/incomplete")
     mark_status = input().lower()
 
     while mark_status not in valid_mark:
@@ -81,6 +96,9 @@ def mark_task():
         num["completed"] = False
 
 def delete_task():
+    if not task_list:
+        print("You don't have any tasks")
+        return
     view_task()
     print("Which task would you like to delete or type -1 to exit")
     while True:
@@ -89,14 +107,14 @@ def delete_task():
             task_num2 = task_num - 1
             if task_num == -1:
                 return
-            elif task_num2 < len(task_list):
+            elif task_num2 < len(task_list) and task_num2 >= 0:
                 break
             else:
                 print("Not a valid task number")
 
         except ValueError:
             print("Please enter a valid task number.")
-    del task_list[task_num]
+    del task_list[task_num2]
 
 def exit_menu():
     global running
@@ -109,11 +127,20 @@ actions = {
     4: delete_task,
     5: exit_menu
 }
+    
+try:
+    with open(file_path, "r") as file:
+        task_list = json.load(file)
+except FileNotFoundError:
+    task_list = []
+
+    with open(file_path, "w") as file:
+        json.dump(task_list, file, indent=4)
 
 while running:
+
     selected = choose_action()
     actions[selected]()
 
-
-with open(file_path, "w") as file:
-    json.dump(task_list, file, indent=4)
+    with open(file_path, "w") as file:
+        json.dump(task_list, file, indent=4)
